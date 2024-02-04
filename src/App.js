507d4eb566aaa7e69812065d94c38b124d989f7d
suraj gaud <gaudsuraj1999@gmail.com>
2024-02-04 18:47:14 +0530

    hook-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Product from './component/Product';
+import Kuchhabhi from './Kuchhabhi';
 
 function App() {
   return (
     <>
       <h1>ReactJS Hook</h1>
-      <Product/>
+      {/* <Product/> */}
+      <Kuchhabhi/>
     </>
   );
 }
---
+++ src/Kuchhabhi.jsx
@@ -0,0 +1,7 @@
+export default function Kuchhabhi(){
+    return(
+        <div>
+            <button>Click me</button>
+        </div>
+    )
+}
